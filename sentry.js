8ef67eff1040103cf03d353ccdcf0c01f300038d
Marcela Karafizievová <62770219+CoffeeladyCZ@users.noreply.github.com>
2025-10-15 14:36:56 +0200

    Fix #11: Add Sentry configuration (#15)

---
+++ sentry.js
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  integrations: [],
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Dashboard } from "./components/Dashboard";
+import { ErrorBoundary } from "react-error-boundary";
+
 import "./App.css";
+import * as Sentry from "@sentry/react";
 
 function App() {
+  const logError = (error: Error) => {
+    Sentry.captureException(error);
+  };
+
   return (
     <>
-      <div>
+      <ErrorBoundary fallbackRender={() => null} onError={logError}>
         <Dashboard />
-      </div>
+      </ErrorBoundary>
     </>
   );
 }
