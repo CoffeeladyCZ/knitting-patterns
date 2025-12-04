575854473ccf4c16a54d2e3b158ad1203cf7482a
Marcela Karafizievova <marcela.karafizievova@gmail.com>
2025-12-04 11:31:44 +0100

    Fix #35: Add localization

--- src/components/patterns/PatternCard.tsx
+++ src/components/patterns/PatternCard.tsx
@@ -1,8 +1,10 @@
 import { Card } from "../component-library/Card";
 import type { Pattern } from "../../api/ravelry/types";
 import { Badge } from "../component-library/Badge";
+import { useTranslation } from "react-i18next";
 
 export const PatternCard = ({ pattern }: { pattern: Pattern }) => {
+  const { t } = useTranslation();
   return (
     <Card className="bg-orange/50 hover:bg-orange/70 gap-2 border-orange/50">
       <Badge
@@ -10,7 +12,7 @@ export const PatternCard = ({ pattern }: { pattern: Pattern }) => {
         size="sm"
         className="self-end"
       >
-        {pattern.free ? "Free" : "Paid"}
+        {pattern.free ? t("patterns.card.free") : t("patterns.card.paid")}
       </Badge>
       <Card.Header>
         <Card.Header.Title>{pattern.name}</Card.Header.Title>
@@ -20,7 +22,9 @@ export const PatternCard = ({ pattern }: { pattern: Pattern }) => {
             alt={pattern.name}
             className="w-full h-48 object-cover"
           />
-          <p>Designer: {pattern.designer?.name}</p>
+          <p>
+            {t("patterns.card.designer")}: {pattern.designer?.name}
+          </p>
         </Card.Content>
       </Card.Header>
     </Card>
--- src/components/yarns/Dashboard.tsx
+++ src/components/yarns/Dashboard.tsx
@@ -1,16 +1,18 @@
 import { useGetYarns } from "../../api/ravelry/hooks";
+import { useTranslation } from "react-i18next";
 
 export const Yarns = () => {
   const { data, isLoading, isError, error } = useGetYarns();
+  const { t } = useTranslation();
 
   console.log(data);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>{t("loading")}</div>;
   }
 
   if (isError) {
-    return <div>Error: {error?.message}</div>;
+    return <div>{t("error", { error: error?.message })}</div>;
   }
-  return <div>Yarns</div>;
+  return <div>{t("yarns.title")}</div>;
 };
